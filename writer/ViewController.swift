fc240652af8f7482b654086eb7405eb2ae840f26
Jake Bloom <jake@steady.health>
2020-07-31 15:42:14 -0700

    Initial Commit

---
+++ writer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  writer
+//
+//  Created by Jake Bloom on 31/7/20.
+//  Copyright © 2020 Jake Bloom. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
